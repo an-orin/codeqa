3d41eb8576e941e66113b0e39390621f4dc6ab0f
An Orin <100474878+an-orin@users.noreply.github.com>
2023-12-10 15:40:19 +0700

    Update 1.7.php

--- 1.7.php
+++ 1.7.php
@@ -1,19 +1,26 @@
-<?php
 error_reporting(0);
 session_start();
+
+
 date_default_timezone_set("Asia/Ho_Chi_Minh");
-$do="\033[1;91m";
-$den="\033[1;33m";
-$tim="\033[1;34m";
-$luc="\033[1;92m";
-$trang="\033[1;97m";
-$vang = "\033[1;93m";
+$Ngay=`date "+%d/%m/%Y"`;
+
+$xanh="\033[1;32m";
+$den = "\033[1;90m";
+$do = "\033[1;91m";
+$luc = "\033[1;92m";
+$vang = "\033[1;33m";
 $hong = "\033[1;95m";
-$xnhac = "\033[1;36m";
-$xduong = "\033[1;96m";
-/*** [ Copyright © 2023 DragonX ] ***/
-$dragonx = $do."[".$trang ."=.=".$do."] ".$trang."=> ";
-/***[ Banner ]***/
+$trang = "\033[1;97m";
+$do="\033[1;91m";
+$van = $do."[".$trang ."=.=".$do."] ".$trang."=> ";
+$vanv = "".$do."➤➤ ".$luc;
+
+$tong_id = [];
+$tong_name = [];
+
+$tt="0";
+$dem = "0";
 $banner = "
 \033[1;34m  █████╗ ███╗   ██╗     ██████╗ ██████╗ ██╗███╗   ██╗
 \033[1;37m ██╔══██╗████╗  ██║    ██╔═══██╗██╔══██╗██║████╗  ██║
@@ -22,13 +29,13 @@ $banner = "
 \033[1;34m ██║  ██║██║ ╚████║    ╚██████╔╝██║  ██║██║██║ ╚████║
 \033[1;37m ╚═╝  ╚═╝╚═╝  ╚═══╝     ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝
 \033[1;31m────────────────────────────────────────────────────────────
-\033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;33mTOOL SHARE ẢO ĐA PRO5 
+\033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;33mTOOL BUFF FOLLOW BẰNG PROFILE
 \033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;35mADMIN: \033[1;36mAN ORIN
 \033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;36mFB: \033[1;31manorintool970
 \033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;32mBOX SUPPORT: \033[1;37mhttps://zalo.me/g/dpfbxq529
 \033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;34mYOUTUBE: \033[1;37mhttps://youtube.com/@AnOrinTool970
 \033[1;31m────────────────────────────────────────────────────────────\n";
-@system('clear');
+@system("clear");
 /***[ Clear + Thông Số Admin ]***/
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'LIN') { 
     @system('clear'); 
@@ -36,129 +43,171 @@ if (strtoupper(substr(PHP_OS, 0, 3)) === 'LIN') {
     @system('cls'); 
 }
 for($i = 0; $i < strlen($banner); $i++){echo $banner[$i];usleep(500);}
-echo $dragonx.$luc."Nhập Cookie Facebook : \033[1;33m";
-$cookie = trim(fgets(STDIN));
-$token = gettoken($cookie);
-if ($token == 'die'){
-   exit($dragonx.$do."Cookie Die!!");
+
+
+echo"\033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;32mNhập Cookie Facebook Có Pgae PROFILE: ".$vang;
+$ck = trim(fgets(STDIN));
+##$ck = "sb=-yoyYwSmR5EpHsjfDBFEC3U5;datr=-yoyY67-L94j4wRGiGjtFOqz;m_pixel_ratio=2;fr=03sQxO3ZVYnUGp8Kd.AWWYteCXWBocwHXSDpKUGNEc5BQ.BjYMgr.Cg.AAA.0.0.BjbR0a.AWWJsKOzMM4;c_user=100087598114269;xs=43%3AcuGknhNow3YuQA%3A2%3A1668095258%3A-1%3A11309;fbl_ci=2576606119140499;m_page_voice=100087598114269;presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1668325698022%2C%22v%22%3A1%7D;x-referer=eyJyIjoiL250L3NjcmVlbi8%2FcGFyYW1zPSU3QiU3RCZwYXRoPSUyRnBhZ2VzJTJGbnRfbGF1bmNocG9pbnRfcmVkZXNpZ24lMkZob21lc2NyZWVuJTJGbXRvdWNoJTJGJnBhaXB2PTAmZWF2PUFmYi1WbU9rcmxWcy1rWjdkWHRkZ0ZLNXRpd1pPUy1VQUtzbGdjZW1qYS1CR0lQcXk2YWt3SFRBWEoxSzJOLVA0d0kiLCJoIjoiL250L3NjcmVlbi8%2FcGFyYW1zPSU3QiU3RCZwYXRoPSUyRnBhZ2VzJTJGbnRfbGF1bmNocG9pbnRfcmVkZXNpZ24lMkZob21lc2NyZWVuJTJGbXRvdWNoJTJGJnBhaXB2PTAmZWF2PUFmYi1WbU9rcmxWcy1rWjdkWHRkZ0ZLNXRpd1pPUy1VQUtzbGdjZW1qYS1CR0lQcXk2YWt3SFRBWEoxSzJOLVA0d0kiLCJzIjoibSJ9;dpr=2;locale=vi_VN;fbl_st=100425614%3BT%3A27845539;fbl_cs=AhCNaVvEdO89SOijXV%2Fm8gOmGGlqajU4QzIwamI1Z1pENnc2NThRSERuYg;vpd=v1%3B760x360x2";
+echo $van."\033[1;32mĐang Check Cookie...!";
+
+$sb_1 = explode(';', explode('sb=', $ck)[1])[0];
+$sb_2 = explode(';', explode('datr=', $ck)[1])[0];
+$sb_3 = explode(';', explode('c_user=', $ck)[1])[0];
+$sb_4 = explode(';', explode('xs=', $ck)[1])[0];
+$sb_5 = explode(';', explode('fr=', $ck)[1])[0];
+$sb_6 = explode(';', explode('wd=', $ck)[1])[0];
+$id_pro = get_pro5($ck);
+
+@system("clear");
+/***[ Clear + Thông Số Admin ]***/
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'LIN') { 
+    @system('clear'); 
+} else { 
+    @system('cls'); 
 }
-echo $dragonx.$luc."Nhập ID Bài viết : \033[1;33m";
-$id_post = trim(fgets(STDIN));
-$get1 = req('https://graph.facebook.com/me/accounts?access_token=' . $token, $cookie);
-$json_get = json_decode($get1, true);
-$stt = 0;
-echo "\033[1;31m────────────────────────────────────────────────────────────\n";
-foreach ($json_get["data"] as $get) {
-    $stt++;
-    echo $dragonx.$do."[".$vang.$stt.$do."] ".$luc."Page : ".$vang.$get['name'].$trang." | ".$luc."ID : ".$vang.$get['id']."\n";
+for($i = 0; $i < strlen($banner); $i++){echo $banner[$i];usleep(500);}
+
+
+$sl = count($id_pro);
+echo $van."\033[1;32mTÌM Thấy : ".$trang.$sl.$luc." PAGE PROFILE \n";
+
+while (true){
+	$so_i = $so_i +1 ;
+	echo $van."\033[1;32mNhập ID Facebook Cần Follow Thứ ".$do.$so_i.$luc." : ".$vang;
+	$idddd = trim(fgets(STDIN));
+	if (!$idddd){
+		break;
+	}else{
+	array_push($tong_id,$idddd);
+	}
 }
-echo "\033[1;31m────────────────────────────────────────────────────────────\n";
-echo $dragonx.$do."[".$vang."Lưu Ý".$do."]"." Chỉ Chọn Page Profile \n";
-echo $dragonx . $luc . "Nhập số ví dụ (" . $trang . "1+2+3" . $luc . ")\n";
-echo $dragonx . $luc . "Nhập Số Page Để Chạy Nhiệm Vụ: $vang";
-$chon_page = trim(fgets(STDIN));
-$chon_page_arr = explode('+', $chon_page); 
-$kho_token_page = [];
-foreach ($json_get["data"] as $index => $get) {
-    if (in_array($index + 1, $chon_page_arr)) { 
-        $token_page = array($get['name'],$get['id'],$get['access_token']);
-        array_push($kho_token_page, $token_page);
+while (true){
+	for ($so_id = 0; $so_id < $so_i ; $so_id++) {
+		$so_d = $so_i -2 ;
+		$id_fl = $tong_id[$so_id];
+		echo"\033[1;31m────────────────────────────────────────────────────────────\n";
+		echo"\033[1;32mĐang Buff Bằng ID : ".$vang.$id_fl." \n";
+		echo"\033[1;31m────────────────────────────────────────────────────────────\n";
+		for ($so = 0; $so < $sl ; $so++) {
+			$id_5 = $id_pro[$so]["profile"]["id"];
+			$name = $id_pro[$so]["profile"]["name"];
+			$ck_pro5 = "sb=".$sb_1."; datr=".$sb_2."; c_user=".$sb_3."; wd=".$sb_6."; xs=".$sb_4."; fr=".$sb_5."; i_user=".$id_5.";";
+			$fl = follow($ck_pro5, $id_5, $id_fl);
+			$tt = $tt +1;
+    		echo "".$do."[".$BBlue.$tt.$do."] ".$luc.$id_5.$do." | ".$trang.$name.$do." | ".$vang."FOLLOW".$do." | ".$luc.$id_fl.$do." |\n";
+    	}
+    if ( $so_d == $so_id ){
+    	echo"\033[1;31m[\033[1;37m=.=\033[1;31m] \033[1;37m=> \033[1;32mFOLLOW SUCCESS \n";
+    	exit();
     }
+	}### trong
+
+}##ngoài 
+
+function get_pro5($ck){
+$url_1 = "https://mbasic.facebook.com/";
+$tsm = array(
+'authority:mbasic.facebook.com',
+        'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language:vi,en;q=0.9,vi-VN;q=0.8,fr-FR;q=0.7,fr;q=0.6,en-US;q=0.5',
+        'cache-control:max-age=0',
+        'cookie:'.$ck,
+        'origin:https://www.facebook.com',
+        'referer:https://www.facebook.com',
+        'sec-ch-ua:"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+        'sec-ch-ua-mobile:?0',
+        'sec-ch-ua-platform:"Windows"',
+        'sec-fetch-dest:document',
+        'sec-fetch-mode:navigate',
+        'sec-fetch-site:none',
+        'sec-fetch-user:?1',
+        'upgrade-insecure-requests:1',
+        'user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+);
+$mr = curl_init();
+curl_setopt_array($mr, array(
+  CURLOPT_PORT => "443",
+  CURLOPT_URL => "$url_1",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_SSL_VERIFYPEER => false,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => $tsm));
+$mr2 = curl_exec($mr); curl_close($mr);
+$_SESSION['fb_dtsg'] = explode('"', explode('<input type="hidden" name="fb_dtsg" value="', $mr2)[1])[0];
+$_SESSION['jazoest'] = explode('"', explode('<input type="hidden" name="jazoest" value="', $mr2)[1])[0];
+
+$url_2 = "https://www.facebook.com/api/graphql/";
+$data = array(
+'fb_dtsg' => $_SESSION['fb_dtsg'] ,
+'jazoest' => $_SESSION['jazoest'],
+'variables' => '{"showUpdatedLaunchpointRedesign":true,"useAdminedPagesForActingAccount":false,"useNewPagesYouManage":true}',
+'doc_id' => '5300338636681652',
+);
+$mr = curl_init();
+curl_setopt_array($mr, array(
+  CURLOPT_PORT => "443",
+  CURLOPT_URL => "$url_2",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_SSL_VERIFYPEER => false,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => $data,
+  CURLOPT_HTTPHEADER => $tsm));
+$mr3 = curl_exec($mr); curl_close($mr);
+$js = json_decode($mr3,true);
+$id_pro = $js['data']['viewer']['actor']['profile_switcher_eligible_profiles']['nodes'];
+
+$sl = count($id_pro);
+
+return $id_pro ;
 }
-echo $dragonx.$luc."Chạy Bao Nhiêu Share Thì Đổi Page : \033[1;33m";
-$dung_doi_page = trim(fgets(STDIN));
-echo $dragonx.$luc."Chạy Bao Nhiêu Share Thì Dừng : \033[1;33m";
-$dung = trim(fgets(STDIN));
-@system('clear');
-/***[ Clear + Thông Số Admin ]***/
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'LIN') { 
-    @system('clear'); 
-} else { 
-    @system('cls'); 
-}
-for($i = 0; $i < strlen($banner); $i++){echo $banner[$i];usleep(500);}
-echo $dragonx.$luc."Tìm Thấy : \033[1;33m".count($kho_token_page)." Page Profile\n";
-echo "\033[1;31m────────────────────────────────────────────────────────────\n";
-echo $dragonx.$luc."Tên Page : ".$vang.$kho_token_page[0][0]."\n";
-echo $dragonx.$luc."ID Page : ".$vang.$kho_token_page[0][1]."\n";
-echo "\033[1;31m────────────────────────────────────────────────────────────\n";
-$tt = 0;
-while(true){
-for ($x = 0; $x < count($kho_token_page); $x++) {
-$tokenpage = $kho_token_page[$x][2];
-$share = sharepro5($id_post, $tokenpage, $cookie);
-$id_s = $share["id"] ;
-if (!$id_s){
-	echo $dragonx.$do." Share Thất Bại ! Kiểm Tra Lại Page : ".$vang.$kho_token_page[$x][0].$do." Hoặc ID Bài Viết \n";
-	array_splice($kho_token_page, $x, 1);
-} else {
-$gio = date("H:i");
-$tt++;
-echo "".$do." | ".$tim.$tt.$do." | ".$luc.$gio.$do." | ".$trang."SHARE".$do." | ".$vang.$id_s.$do." | \n";
-if ($tt % $dung_doi_page == 0) {
-array_splice($kho_token_page, $x, 1);
-if (count($kho_token_page) < 1){
-    echo "\033[1;31m────────────────────────────────────────────────────────────\n";
-    echo $dragonx . $do . "Toàn Bộ Page Bạn Chọn Đã Hết !!\n";
-    echo $dragonx."Share Thành Công Tổng : $tt Share\n";
-    echo $dragonx.$xnhac."Cảm ơn Bạn Đã Sử Dụng Tool By DragonX\n";
-    echo $dragonx.$xnhac."Có Gì Thắc Mắc Hay Muốn Đề Xuất Gì Thì Bảo Quản Trị Viên Trong Box Zalo Nhé . Cảm Ơn Bạn Rất Nhiều!\n";
-    echo "\033[1;31m────────────────────────────────────────────────────────────\n";
-    exit;
-} else {
-echo "\033[1;31m────────────────────────────────────────────────────────────\n";
-echo $dragonx.$luc."Tên Page : ".$vang.$kho_token_page[$x][0]."\n";
-echo $dragonx.$luc."ID Page : ".$vang.$kho_token_page[$x][1]."\n";
-echo "\033[1;31m────────────────────────────────────────────────────────────\n";
-}
-}
-if ( $tt == $dung ){
-    echo "\033[1;31m────────────────────────────────────────────────────────────\n";
-	echo $dragonx."Share Thành Công Tổng : $tt Share\n";
-    echo $dragonx.$xnhac."Cảm ơn Bạn Đã Sử Dụng Tool By DragonX\n";
-    echo $dragonx.$xnhac."Có Gì Thắc Mắc Hay Muốn Đề Xuất Gì Thì Bảo Quản Trị Viên Trong Box Zalo Nhé . Cảm Ơn Bạn Rất Nhiều!\n";
-    echo "\033[1;31m────────────────────────────────────────────────────────────\n";
-	exit;
-}
-}
-}
+function follow($ck, $id_page, $id_fl){
+$url = "https://www.facebook.com/api/graphql/";
+$data = array(
+            'fb_dtsg' => $_SESSION['fb_dtsg'] ,
+            'jazoest' => $_SESSION['jazoest'],
+            'fb_api_caller_class' =>'RelayModern',
+            'fb_api_req_friendly_name' => 'CometUserFollowMutation',
+            'variables' => '{"input":{"attribution_id_v2":"ProfileCometTimelineListViewRoot.react,comet.profile.timeline.list,via_cold_start,1667114418950,431532,190055527696468,","subscribe_location":"PROFILE","subscribee_id":"'.$id_fl.'","actor_id":"'.$id_page.'","client_mutation_id":"1"},"scale":1}',
+            'server_timestamps' => 'true',
+            'doc_id' => '5032256523527306',
+);
+$tsm = array(
+                'authority:www.facebook.com',
+                'accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-language:vi',
+                'cookie:'.$ck,
+                'sec-ch-prefers-color-scheme:light',
+                'sec-ch-ua:"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+                'sec-ch-ua-mobile:?0',
+                'sec-ch-ua-platform:"Windows"',
+                'sec-fetch-dest:document',
+                'sec-fetch-mode:navigate',
+                'sec-fetch-site:none',
+                'sec-fetch-user:?1',
+                'upgrade-insecure-requests:1',
+                'user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+                'viewport-width:1366',
+            );
+$mr = curl_init();
+curl_setopt_array($mr, array(
+  CURLOPT_PORT => "443",
+  CURLOPT_URL => "$url",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_SSL_VERIFYPEER => false,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => $data,
+  CURLOPT_HTTPHEADER => $tsm));
+$mr3 = curl_exec($mr); curl_close($mr);
+$js = json_decode($mr3,true);
+
+$check = $js["data"]["actor_subscribe"]["subscribee"]["subscribe_status"];
+$id_1 = $js["data"]["actor_subscribe"]["subscribee"]["id"];
+
+	return $mr3;
+
 
 }
 
-
-
-
-function sharepro5($id_post, $tokenpage, $cookie){
-
-$url = req("https://graph.facebook.com/me/feed?method=POST&link=https://m.facebook.com/" . $id_post . "&published=0&access_token=" . $tokenpage, $cookie);
-
-$js = json_decode($url,true);
-   return $js;
-}
-function gettoken($cookie) {
-    $getck = file_get_contents("https://key.shopdragonx.com/apiv2/func.php?get_token=EAAG&cookie=".$cookie);
-    return $getck;
-}
-function req($url, $cookie)
-{
-    $ch   = curl_init();
-    curl_setopt_array(
-        $ch,
-        array(
-            CURLOPT_URL => $url,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_FOLLOWLOCATION => TRUE,
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_SSL_VERIFYPEER => 0,
-            CURLOPT_HTTPHEADER =>
-            array(
-                "Cookie:" . $cookie,
-            ),
-            CURLOPT_SSL_VERIFYPEER => FALSE,
-            CURLOPT_ENCODING => TRUE,
-        )
-    );
-    $ch = curl_exec($ch);
-    return $ch;
-}
-?>
